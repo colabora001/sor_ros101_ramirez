af546c57d4404201108d0ebe22ad08b5ad50b6d1
Jose Luis Ramirez Herran <joseistheone@gmail.com>
2022-02-21 23:13:05 -0500

    scriptstested and corrected then success

--- scripts/cylinder_calc.py
+++ scripts/cylinder_calc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Float64
-from ros_tutorial.msg import Cylinder
+from sor_ros101_ramirez.msg import Cylinder
 
 from math import pi
 
@@ -9,34 +9,34 @@ radius=0
 radius_squared = 0
 height = 0
 
-radius_found = false
-radius_squared_found = false
-height_found = false
+radius_found = False
+radius_squared_found = False
+height_found = False
 
 def radius_callback(data):
 	global radius
 	global radius_found
 	radius = data.data
-	radius_found = true
+	radius_found = True
 def radius_squared_callback(data):
 	global radius_squared
 	global radius_squared_found
 	radius_squared = data.data
-	radius_squared_found = true
+	radius_squared_found = True
 def height_callback(data):
 	global height
 	global height_found
 	height = data.data
-	height_found = true
+	height_found = True
 
 def calculate():
 	if radius_found and radius_squared_found and height_found:
 		msg = Cylinder()
-		msg.volume = pi * radius_Square * height
+		msg.volume = pi * radius_squared * height
 		msg.surface_area = 2 * pi * (radius * height + radius_squared)
 		pub.publish(msg)
 
-rospy.ini_node("cylinder_calc")
+rospy.init_node("cylinder_calc")
 rospy.Subscriber("/radius", Float64, radius_callback)
 rospy.Subscriber("/radius_squared", Float64, radius_squared_callback)
 rospy.Subscriber("/height", Float64, height_callback)
